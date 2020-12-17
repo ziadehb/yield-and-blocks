ea16af1911bf65ccc19dd622aed5ef8d41fd75f1
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-17 15:44:01 -0500

    Add files via upload

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,14 @@
 def hello_t(array)
-    if block_given?
-      i = 0
-  
-      while i < array.length
-        yield(array[i])
-        i = i + 1
-      end
-  
-      array
-    else
-      puts "Hey! No block was given!"
+  if block_given?
+    i = 0
+    
+    while i < array.length
+      yield array[i]
+      i = i + 1
     end
-  end 
+    array
+
+  else
+    puts "Hey! No block was given!"
+  end
+end 
